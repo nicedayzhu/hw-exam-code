f432f90512ac29d0e4e76148bef8f67e89d4c78b
nicedayzhu <wobudangxuezhang@gmail.com>
2019-08-29 11:15:48 +0800

    修改

--- 求字符串所有子串-连续的.py
+++ 求字符串所有子串-连续的.py
@@ -21,7 +21,7 @@ def cutStr(str):
 def cutStr2(str):
     ls_cut = []
     for i in range(len(str)):
-        for j in range(i + 1, len(str)):
+        for j in range(i + 1, len(str)+1):
             sub = str[i:j]
             ls_cut.append(sub)
     return ls_cut
